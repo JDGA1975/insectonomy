e0b4fdbaa8b5f6c8eaee698c3c67ee3e61bfebbd
JDGA1975 <56745103+JDGA1975@users.noreply.github.com>
2023-07-10 21:56:29 -0500

    Add files via upload

--- insectonomy.py
+++ insectonomy.py
@@ -1,35 +1,36 @@
 import streamlit as st
 import pandas as pd
-st.title("Página Principal Insectonomy")
-st.subheader("Características generales")
-st.text("Parrafo de html")
-st.markdown("## **Hello** world. *juan*")
-st.markdown("> Bloque")
-st.markdown("[Google is here](google.com)")
+import re
 
-st.code("import pandas as pd")
+st.title("Página Principal Insectonomy")
+st.subheader("Resultados de la bussqueda")
+#st.text("Página para navegar los datos.")
+
 r=[]
+
 tabla=pd.read_csv("General.csv", sep=";")
 tabla2=tabla
 
+    #tabla=tabla[tabla["Exotic or Native to CO"]=="Native"]
 
-            #tabla=tabla[tabla["Exotic or Native to CO"]=="Native"]
-nat=st.checkbox("Native",value=True)
-exo=st.checkbox("Exotic",value=True)
+with st.sidebar:
+    st.subheader("Parámetros de busqueda")
+    st.text("Especie nativa o exótica en Colombia?")
+    nat=st.checkbox("Native",value=True)
+    exo=st.checkbox("Exotic",value=True)
 
+#Selección de la columna exotic
 if nat and not exo:
-        st.header("Marco")
         tabla2=tabla[tabla["exotic"]=="Native"]
         
 else:
     if not nat and exo:
-        st.header("Polo")
         tabla2=tabla[tabla["exotic"]=="Exotic"]
     else:
         tabla2=tabla
  
+#Seleccion de la zona
 l=tabla.zone.unique()
-import re
 places=[]
 for i in l:
     j=re.split(",| and", i)
@@ -38,32 +39,53 @@ for i in l:
         k=k.strip()
         if k not in places:
             places.append(k.strip())
-
 places.sort()
-lugares=st.multiselect("Seleccione areas de interés",places)
+with st.sidebar:
+    st.text("Selecciones la(s) zona(s)")
+    lugares=st.multiselect("Seleccione areas de interés",places)
 
-lugares
-#r.append(region)
-#r
 query=""
 for i in lugares:
     query=query+i+"|"
-#    tabla[tabla["zone"].str.contains("America|Europe")]["zone"]
 query=query[0:-1]
 tabla2=tabla2[tabla2["zone"].str.contains(query)]
 
-l=tabla.zone.unique()
-tabla2
-#CLIMAS
+
+
+ 
+#Seleccion de la "Biogeographic realm
+ll=tabla.Biogeographicrealm.unique()
+realms=[]
+for i in ll:
+    j=re.split(",| and", i)
+ #       for k in j:
+    for k in j:
+        k=k.strip()
+        if k not in realms:
+            realms.append(k.strip())
+realms.sort()
+with st.sidebar:
+    reinos=st.multiselect("Seleccione los reinos de interés",realms)
+
+query=""
+realms
+for i in reinos:
+    query=query+i+"|"
+query=query[0:-1]
+query
+tabla2=tabla2[tabla2["Biogeographicrealm"].str.contains(query)]
+
+
+#Seleccion del CLIMA
 cl=tabla.Climate.unique()
-climas=st.multiselect("Seleccione los climas de interés",cl)
+with st.sidebar:
+    st.text("Selección del clima")
+    climas=st.multiselect("Seleccione los climas de interés",cl)
 query=""
 for i in climas:
     query=query+i+"|"
 #    tabla[tabla["zone"].str.contains("America|Europe")]["zone"]
 query=query[0:-1]
-climas
-
 tabla2=tabla2[tabla2["Climate"].str.match(query)]
 
 
